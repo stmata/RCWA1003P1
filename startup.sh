d6b1c9802072d3fe4d399f435c6aa669e078a12f
stmata <108939392+stmata@users.noreply.github.com>
2024-10-04 16:25:52 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@
 # Move into the correct directory where the FastAPI app is located
 
 # Run the FastAPI + Dash app using Uvicorn
-uvicorn fastapi_app.main:app --host 0.0.0.0 --port 8000 --workers 1
+uvicorn fastapi_app.main:app --host 0.0.0.0 --port 8005 --workers 1
