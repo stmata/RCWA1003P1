e5100cc5fb125882e5f5757c6f478f053442bd4a
stmata <108939392+stmata@users.noreply.github.com>
2024-10-11 14:10:12 -0400

    Added connection to weather API
    Updated home, login, and dash_app

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -7,6 +7,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.wsgi import WSGIMiddleware
 from fastapi.templating import Jinja2Templates
 import uvicorn
+import requests
 from dash_app import app as app_dash
 # Create fastAPI app
 
@@ -21,27 +22,39 @@ static_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "static"))
 app.mount("/static", StaticFiles(directory=static_dir))
 # Set up Jinja2 template for rendering HTML files
 templates = Jinja2Templates(directory=templates_dir)
-
+# External API URL (replace with the actual URL)
+EXTERNAL_API_URL = "http://127.0.0.1:8021/info"
+ 
+def get_external_info():
+    try:
+        response = requests.get(EXTERNAL_API_URL)
+        return response.json()  # Convert response to JSON
+    except Exception as e:
+        return {"date": "N/A", "time": "N/A", "weather": {"city": "Unknown", "temperature": "N/A", "description": "N/A"}}
+ 
 # In-memory user storage for login
 users = {"admin": "password"}
 
 # Define routes
 @app.get("/")
 async def home_page(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    info = get_external_info()
+    return templates.TemplateResponse("home.html", {"request": request, "info": info})
 
 @app.get("/login")
 async def login_page(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
+    info = get_external_info()
+    return templates.TemplateResponse("login.html", {"request": request, "info":info})
 
 @app.post("/login")
 async def login(request: Request, username: str = Form(...), password: str = Form(...)):
+    info = get_external_info()
     if username in users and users[username] == password:
         # Redirect to the dashboard generated with dash upon success
         response = RedirectResponse(url='/dashboard', status_code=302)
         response.set_cookie(key="Authorization", value="Bearer Token", httponly=True)
         return response
-    return templates.TemplateResponse("login.html", {"request": request, "error": "Invalid credentials"})
+    return templates.TemplateResponse("login.html", {"request": request, "error": "Invalid credentials", "info": info})
 
 @app.get("/logout")
 async def logout():
